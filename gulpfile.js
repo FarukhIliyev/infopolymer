7bf06dd08d9cce41112399484d6f83fec39d54a3
Farukh Iliyev <iliyevfarukh@gmail.com>
2021-04-13 13:32:56 +0600

    layout 5.7 index changed

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("source/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
