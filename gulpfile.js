3277eaf3198d39ea30084acce0a06ae308af21fb
Farukh Iliyev <iliyevfarukh@gmail.com>
2021-04-13 12:15:56 +0600

    layout 5.6

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
